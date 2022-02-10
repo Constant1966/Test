9768c919f6eb31ff4225ce00b23dca3bd30a3b42
Constant1966 <constantlorvenson@gmail.com>
2022-02-10 00:51:15 -0500

    modifier

--- src/Lab3/Challenge26.java
+++ src/Lab3/Challenge26.java
@@ -4,6 +4,29 @@ import java.util.Scanner;
 public class Challenge26 {
 
 	public static void main(String[] args) {
+		Scanner user = new Scanner(System.in);
+		
+		System.out.println("Please enter a word");
+		String word = user.nextLine();
+		
+		String value = word.substring(0, 1);
+		int len = word.length();
+		String rest = word.substring(1, len);
+		
+		if (value.equals("a") || value.equals("e") || value.equals("i") || value.equals("o") || value.equals("u") || value.equals("y"))
+		{
+			String newword = word + "way";
+			System.out.println(newword);
+		}
+		else
+		{
+			String newword = rest + value + "ay";
+			System.out.println(newword);
+		}
+		
+//		String first = String.valueOf(word);
+//		System.out.println(first);
+		
 
 	}
 
